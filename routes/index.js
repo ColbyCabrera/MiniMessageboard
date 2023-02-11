9568779fff6e6fa9d8ad64214c1365e8018b5ff9
ColbyCabrera <gdthyispro@gmail.com>
2023-02-11 16:53:59 -0500

    Add new message route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+let messages = [
+  {
+    text: "hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages});
 });
 
+router.post('/new', function(req, res) {
+
+  const user = req.body.message_name;
+  const text = req.body.message_text;
+
+  messages.push({ text, user, added: new Date() });
+
+  res.redirect('/');
+});
+
+
 module.exports = router;
+
